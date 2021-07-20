3433cab3337c0d060619b7f094366036cd497c32
boren <liu_yichun@163.com>
2021-07-20 17:47:35 +0800

    初始化

---
+++ src/main/java/com/design/patterns/DesignPatternsApplication.java
@@ -0,0 +1,17 @@
+package com.design.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author boren
+ * @date 2021/07/20
+ */
+@SpringBootApplication
+public class DesignPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsApplication.class, args);
+    }
+
+}
